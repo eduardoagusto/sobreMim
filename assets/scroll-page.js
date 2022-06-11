56b88f6800b59a7dc44a896cc2486990a794f81a
Eduardo Augusto <84025690+eduardoagusto@users.noreply.github.com>
2022-06-11 19:06:13 -0300

    scroll-page

---
+++ assets/scroll-page.js
@@ -0,0 +1,16 @@
+ScrollReveal({
+    reset: true,
+    distance: '50px',
+    duration:2000,
+    delay:400,
+});
+ScrollReveal().reveal('.profile-img',{delay:40,origin:'right',distance: '1000px',duration:2000})
+ScrollReveal().reveal('.name-sub',{delay:40,origin:'left',distance: '1000px',duration:2000})
+ScrollReveal().reveal('.text-block',{delay:600,origin:'left',distance: '1000px',duration:2200})
+ScrollReveal().reveal('.title-project',{delay:100,origin:'bottom',distance: '60px',duration:2000})
+ScrollReveal().reveal('.project-1',{delay:200,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-2',{delay:300,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-3',{delay:400,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-4',{delay:500,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-5',{delay:600,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-6',{delay:700,origin:'left',distance: '300px',duration:2000})
