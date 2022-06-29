82b2fcd3542f3e31d1d687029f4182503cc6d040
Eduardo Augusto <84025690+eduardoagusto@users.noreply.github.com>
2022-06-29 14:11:45 -0300

    add functions

--- assets/scroll-page.js
+++ assets/scroll-page.js
@@ -5,12 +5,11 @@ ScrollReveal({
     delay:400,
 });
 ScrollReveal().reveal('.profile-img',{delay:40,origin:'right',distance: '1000px',duration:2000})
-ScrollReveal().reveal('.name-sub',{delay:40,origin:'left',distance: '1000px',duration:2000})
-ScrollReveal().reveal('.text-block',{delay:600,origin:'left',distance: '1000px',duration:2200})
-ScrollReveal().reveal('.title-project',{delay:100,origin:'bottom',distance: '60px',duration:2000})
-ScrollReveal().reveal('.project-1',{delay:200,origin:'left',distance: '300px',duration:2000})
-ScrollReveal().reveal('.project-2',{delay:300,origin:'left',distance: '300px',duration:2000})
-ScrollReveal().reveal('.project-3',{delay:400,origin:'left',distance: '300px',duration:2000})
-ScrollReveal().reveal('.project-4',{delay:500,origin:'left',distance: '300px',duration:2000})
-ScrollReveal().reveal('.project-5',{delay:600,origin:'left',distance: '300px',duration:2000})
-ScrollReveal().reveal('.project-6',{delay:700,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-1',{delay:100,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-2',{delay:200,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-3',{delay:300,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-4',{delay:400,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-5',{delay:500,origin:'left',distance: '300px',duration:2000})
+ScrollReveal().reveal('.project-6',{delay:600,origin:'left',distance: '300px',duration:2000});
+ScrollReveal().reveal('.pokedex',{delay:50,origin:'left',distance: '800px',duration:2000});
+ScrollReveal().reveal('.calculator',{delay:200,origin:'right',distance: '800px',duration:2000});
